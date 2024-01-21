653ea47cc0bf821b1cf2b4decdddfb58dc3c06c8
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-01-21 14:49:55 -0300

    Update index.php

--- desafio1/index.php
+++ desafio1/index.php
@@ -3,10 +3,9 @@
 abrir container com imagem teste
 docker run -dp 8080:80 -v C:\Users\ilsidonia\Desktop\estagioimply\desafios\desafio1:/var/www/html --name desafioum php:8.3.1-apache
 
-docker cp /home/imply/Área\ de\ Trabalho/desafios/desafio1 compassionate_antonelli:/var/www/html/
+docker cp /home/imply/Área\ de\ Trabalho/desafios/desafio1 desafioum:/var/www/html/
   
-
-docker exec -it compassionate_antonelli php /var/www/html/desafio1/index.php
+docker exec -it desafioum php /var/www/html/desafio1/index.php
    */
   require_once __DIR__.'/pedeDimensao.php';
   require_once __DIR__.'/testaSeMultiplica.php';
