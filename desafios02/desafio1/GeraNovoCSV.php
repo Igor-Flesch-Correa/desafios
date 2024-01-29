069c592609f1522e6fcd35ea2e2f9bfae715286f
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-01-29 11:56:54 -0300

    mudei

--- desafios02/desafio1/GeraNovoCSV.php
+++ desafios02/desafio1/GeraNovoCSV.php
@@ -1,5 +1,6 @@
 <?php
-class GeraNovoCSV {
+class GeraNovoCSV 
+{
     private $file1;
     private $file2;
     private $newFile;
@@ -24,13 +25,29 @@ class GeraNovoCSV {
     }
     //rewind($ponteiro); volta o ponteiro para primeira linha
 
-    public function Gera() {
+    public function Gera() 
+    {
         $file1Data = $this->readCSV($this->file1);
         $file2Data = $this->readCSV($this->file2);
 
-        // Processar apenas a primeira coluna do primeiro arquivo
+        // Logica de filtragem
         $IdProdutos = array_column($file1Data, 0);
+
         $Preco = array_column($file1Data, 2);
+
+        $UltimaData = [];
+
+        foreach ($file2Data as $linha) {
+            $id = $linha[1];
+            $data = $linha[2];
+
+            if (!isset($UltimaData[$id]) || strtotime($data) > strtotime($UltimaData[$id])) {
+                $UltimaData[$id] = $data;
+            }
+
+        
+
+        $ArrayNovo = array_map(null, $IdProdutos, $Preco);//junta os arrai de uma dimensao e cada um vira uma coluna, colocar o resto...
 
 
         // Aqui você pode adicionar lógica adicional para manipular os dados
@@ -44,17 +61,22 @@ class GeraNovoCSV {
       
         
 
-        // Exemplo: Imprimir os valores da primeira coluna do primeiro arquivo
-        foreach ($IdProdutos as $value) {
-            echo "Valor: $value\n";
-            fputcsv($ponteiro2, [$value]);
-
+        // pega os arrays e junta em linhas(array novo)
+        for ($i = 0; $i < count($IdProdutos); $i++) {
+            // Combine o elemento atual de cada coluna em um array
+            $linha = [$IdProdutos[$i], $Preco[$i]];//colocar o resto...
+        
+            fputcsv($ponteiro2, $linha);//escreve
         }
 
-       
         fclose($ponteiro2);
     }
+
+       
+        
 }
+
+
 
 // Uso da classe
 new GeraNovoCSV(__DIR__. '/products.csv',__DIR__.'/orders.csv',__DIR__.'/novo.csv');
