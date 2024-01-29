12e59a1799625134899243f158b08b5c1f2bb5ba
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-01-29 09:54:28 -0300

    gerei

--- desafios02/desafio1/index.php
+++ desafios02/desafio1/index.php
@@ -1,5 +1,6 @@
 <?php
-//docker run -dp 8080:80 -v /home/imply/Área\ de\ Trabalho/desafios/desafio02/desafio1:/var/www/html --name desafioum php:8.3.2-apache
+//docker run -dp 8080:80 -v /home/imply/Área\ de\ Trabalho/desafios/desafios02/desafio1:/var/www/html  --name desafioum php:8.3.2-apache
+
 //docker run -dp 8080:80 -v C:\Users\ilsidonia\Desktop\estagioimply\desafios\desafio02/desafio1:/var/www/html --name desafioum php:8.3.2-apache
 
 
