1e1b07cd31263196168c1523a912e904a1b8d4f5
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-01-30 14:18:27 -0300

    Update index.php

--- desafios02/desafio1/index.php
+++ desafios02/desafio1/index.php
@@ -1,7 +1,7 @@
 <?php
 //docker run -dp 8080:80 -v /home/imply/Área\ de\ Trabalho/desafios/desafios02/desafio1:/var/www/html/projeto  --name desafioum emailsender
 
-//docker run -dp 8080:80 -v C:\Users\ilsidonia\Desktop\estagioimply\desafios\desafio02/desafio1:/var/www/html/projeto --name desafioum emailsender
+//docker run -dp 8080:80 -v C:\Users\ilsidonia\Desktop\estagioimply\desafios\desafios02/desafio1:/var/www/html/projeto --name desafioum emailsender
 //composer require phpmailer/phpmailer 
 include_once 'GeraNovoCSV.php';
 include_once 'EmailSender.php';
