b4e6c9d6f484e0d7be4e343abb43b0b5c269a41f
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-01-30 13:44:17 -0300

    Update index.php

--- desafios02/desafio1/index.php
+++ desafios02/desafio1/index.php
@@ -1,7 +1,7 @@
 <?php
 //docker run -dp 8080:80 -v /home/imply/Área\ de\ Trabalho/desafios/desafios02/desafio1:/var/www/html  --name desafioum php:8.3.2-apache
 
-//docker run -dp 8080:80 -v C:\Users\ilsidonia\Desktop\estagioimply\desafios\desafio02/desafio1:/var/www/html --name desafioum php:8.3.2-apache
+//docker run -dp 8080:80 -v C:\Users\ilsidonia\Desktop\estagioimply\desafios\desafios02\desafio1:/var/www/html --name desafioum php:8.3.2-apache
 
 
 
