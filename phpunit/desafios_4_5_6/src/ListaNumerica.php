71007f91c5c22ce9ca417efa024bd7479cbc870f
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-02-16 11:48:32 -0300

    começo do 4

---
+++ phpunit/desafios_4_5_6/src/ListaNumerica.php
@@ -0,0 +1,34 @@
+<?php
+
+class ListaNumerica
+{
+    protected $numeros = [];
+
+    public function adicionarNumero($numero)
+    {
+        if (!is_numeric($numero)) {
+            throw new InvalidArgumentException("O valor deve ser numérico.");
+        }
+
+        $this->numeros[] = $numero;
+    }
+
+    public function getMaximo()
+    {
+        return max($this->numeros);
+    }
+
+    public function getMinimo()
+    {
+        return min($this->numeros);
+    }
+
+    public function calcularMedia()
+    {
+        if (empty($this->numeros)) {
+            return null;
+        }
+
+        return array_sum($this->numeros) / count($this->numeros);
+    }
+}
---
+++ phpunit/desafios_4_5_6/tests/ListaNumericaTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once 'src/ListaNumerica.php';
+
+class ListaNumericaTest extends TestCase
+{
+    public function testAdicionarNumero()
+    {
+        $lista = new ListaNumerica();
+        $lista->adicionarNumero(5);
+
+        $this->assertEquals([5], $lista->numeros);
+    }
+
+    public function testAdicionarNumeroNaoNumerico()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $lista = new ListaNumerica();
+        $lista->adicionarNumero('não é um número');
+    }
+
+    public function testGetMaximo()
+    {
+        $lista = new ListaNumerica();
+        $lista->adicionarNumero(1);
+        $lista->adicionarNumero(2);
+        $lista->adicionarNumero(3);
+
+        $this->assertEquals(3, $lista->getMaximo());
+    }
+
+    public function testGetMinimo()
+    {
+        $lista = new ListaNumerica();
+        $lista->adicionarNumero(1);
+        $lista->adicionarNumero(2);
+        $lista->adicionarNumero(3);
+
+        $this->assertEquals(1, $lista->getMinimo());
+    }
+
+    public function testCalcularMedia()
+    {
+        $lista = new ListaNumerica();
+        $lista->adicionarNumero(1);
+        $lista->adicionarNumero(2);
+        $lista->adicionarNumero(3);
+
+        $this->assertEquals(2, $lista->calcularMedia());
+    }
+
+    public function testCalcularMediaListaVazia()
+    {
+        $lista = new ListaNumerica();
+
+        $this->assertNull($lista->calcularMedia());
+    }
+}
